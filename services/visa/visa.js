87070aec73d016046cff85a87dc49e02af6e9efe
Илья Попов <ilapopov@MacBook-Air-Ila.local>
2023-10-29 20:45:39 +0300

    add news page

--- services/visa/visa.js
+++ services/visa/visa.js
@@ -1,18 +1,18 @@
-const visaInput = document.getElementById('visa-date');
-const passportInput = document.getElementById('passport-date');
-const button = document.getElementsByClassName('form__button');
+const visaInput = document.querySelector('#visa-date')
+const passportInput = document.querySelector('#passport-date')
+const button = document.querySelector('.form__button')
 
 
 visaInput.addEventListener('input', (event) => {
     const visaExpiryDate = visaInput.value
     console.log(visaExpiryDate)
-});
+})
 
 passportInput.addEventListener('input', (event) => {
     const passportExpiryDate = passportInput.value
     console.log(passportExpiryDate)
-});
+})
 
-button.addEventListener('onclick', (event) => {
+button.addEventListener('click', (event) => {
     
 })
